eb291768a1081591f54e732952e7641ce04027c0
chhaya <chhaya.sharma@ericsson.com>
2021-06-15 21:35:13 +0100

    Stock Price

---
+++ src/com/company/Interval.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Interval {
+    int buy, sell;
+}
---
+++ src/com/company/Price.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Price {
+    /* 7,1 ,5 ,3, 6, 4*/
+
+    // min =1
+
+//    7, 6, 4, 3, 1#
+    // finding minimum no.
+    public static void main(String[] args) {
+
+    }
+}
